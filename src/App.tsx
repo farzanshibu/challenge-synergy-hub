b2efe86d8bf11a2ace26c11f570ba19dcb4221c4
Farzan Shibu <farzanshibu786@gmail.com>
2025-03-17 08:32:11 +0530

    remove the old toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Toaster } from "sonner";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -25,7 +24,6 @@ const App = () => (
     <AuthProvider>
       <TooltipProvider>
         <Toaster richColors position="top-center" />
-        <Sonner />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
